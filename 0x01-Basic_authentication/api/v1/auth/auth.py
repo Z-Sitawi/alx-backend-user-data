ecd716b34710b33df87ef735a67d97821210890c
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-13 19:12:10 +0100

    task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Module of class to manage the API authentication.
+"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ method doc here """
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """ method doc here """
+        return request
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ method doc here """
+        return request
