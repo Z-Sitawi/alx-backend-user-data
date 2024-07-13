50db4b3a6cdc35ebb0208f0b95bb3e69b654f620
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-13 18:59:37 +0100

    task 2

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -21,7 +21,7 @@ def unauthorized():
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def unauthorized():
+def forbidden():
     """ raise a 403 error """
     abort(403)
 
