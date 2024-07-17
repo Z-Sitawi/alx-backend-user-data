a7280ea05f74281ae9e3581fcfa356336d1d4983
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-17 19:19:48 +0100

    task 0

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,11 +4,16 @@ import re
 from typing import List
 
 
+patterns = {
+    'extract': lambda x, y: r'(?P<field>{})=[^{}]*'.format('|'.join(x), y),
+    'replace': lambda x: r'\g<field>={}'.format(x),
+}
+
+
 def filter_datum(
-    fields: List[str], redaction: str, message: str, separator: str
-) -> str:
-    """doc doc doc"""
-    for field in fields:
-        regex = f"{field}=[^{separator}]*"
-        message = re.sub(regex, f"{field}={redaction}", message)
-    return message
+        fields: List[str], redaction: str, message: str, separator: str,
+        ) -> str:
+    """Filters a log line.
+    """
+    extract, replace = (patterns["extract"], patterns["replace"])
+    return re.sub(extract(fields, separator), replace(redaction), message)
