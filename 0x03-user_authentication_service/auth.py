cf024bfd576b9e7854fca1f123f23d3fcec0ab61
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-18 18:32:18 +0100

    task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,2 +1,13 @@
 #!/usr/bin/env python3
 """ Module doc here """
+from bcrypt import gensalt, hashpw
+
+
+def _hash_password(password: str) -> bytes:
+    """
+        hashes a password
+    :param password: the pwd to hash
+    :return: hashed pwd in bytes
+    """
+    salt = gensalt()
+    return hashpw(password.encode('utf-8'), salt)
