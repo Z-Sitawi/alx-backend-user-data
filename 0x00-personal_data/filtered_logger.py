e8692c1285e27184f2af1c7b90f0e56f8a1dca2a
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-18 09:02:14 +0100

    empty files

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,7 +7,7 @@ from typing import List
 def filter_datum(
     fields: List[str], redaction: str, message: str, separator: str
 ) -> str:
-    """doc doc doc"""
+    """ returns the log message obfuscated """
     for field in fields:
         regex = f"{field}=[^{separator}]*"
         message = re.sub(regex, f"{field}={redaction}", message)
