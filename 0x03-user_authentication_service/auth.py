9738a99b42505021400e1b5c2ce48a947940e3f9
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-18 21:17:49 +0100

    task 8

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ Module doc here """
-from bcrypt import gensalt, hashpw
+from bcrypt import gensalt, hashpw, checkpw
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
 from user import User
@@ -21,6 +21,17 @@ class Auth:
             hashed_pwd = str(_hash_password(password))
             user = self._db.add_user(email=email, hashed_password=hashed_pwd)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Checks if a user's login details are valid. """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user is not None:
+                return checkpw(password.encode('utf-8'),
+                               user.hashed_password)
+        except NoResultFound:
+            return False
+        return False
 
 
 def _hash_password(password: str) -> bytes:
