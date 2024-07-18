6d1670dfe43ea960d230586ca09ef0080f804b99
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-18 16:24:12 +0100

    task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 """DB module
 """
-import random
-
 from sqlalchemy import create_engine, tuple_
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
 
 from user import Base, User
