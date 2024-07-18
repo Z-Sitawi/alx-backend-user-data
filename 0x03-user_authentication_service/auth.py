7ebe98e35d63bb1a0bd3268cb78ec588f658a12b
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-18 23:21:10 +0100

    task 13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -55,6 +55,10 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroys a session """
+        self._db.update_user(user_id=user_id, session_id=None)
+
 
 def _hash_password(password: str) -> bytes:
     """
