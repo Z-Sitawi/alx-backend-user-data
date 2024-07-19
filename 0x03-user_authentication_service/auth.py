2e0dfb2c0b03001d2986f234dcf35a72ea89cdc8
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-19 18:43:01 +0100

    task 16

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -59,6 +59,16 @@ class Auth:
         """ Destroys a session """
         self._db.update_user(user_id=user_id, session_id=None)
 
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generate reset password token """
+        try:
+            user = self._db.find_user_by(email=email)
+            unique_id = _generate_uuid()
+            self._db.update_user(user_id=user.id, reset_token=unique_id)
+            return unique_id
+        except NoResultFound:
+            raise ValueError()
+
 
 def _hash_password(password: str) -> bytes:
     """
